9d8a0bfbdbe582713130f8988579f32b99fa23eb
Basem Walid <basemwalid44@gmail.com>
2025-12-19 21:20:26 +0200

    he2

--- client/reddit/src/main.jsx
+++ client/reddit/src/main.jsx
@@ -4,7 +4,7 @@ import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-
+//hello
 root.render(
   <BrowserRouter>
     <App />
