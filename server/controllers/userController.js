18e56f0b7acffa2cf6744f5c0a80def5c9008e69
Basem Walid <basemwalid44@gmail.com>
2025-12-20 03:42:51 +0200

    bismillah

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -9,9 +9,9 @@ const { validationResult } = require('express-validator');
 
 
 const transporter = nodemailer.createTransport({
-  host: 'smtp.resend.com',
-  secure: true,
-  port: 465,
+  host: 'smtp.resend.com',]
+  port: 587,
+  secure: false,
   auth: {
     user: 'resend',
     pass: process.env.EMAIL_PASS,
